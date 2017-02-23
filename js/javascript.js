d322a980f34a4a3566083f3cbba2c6bc1cdc10e9
Benjamin E. Valencia - Bencod <becod@users.noreply.github.com>
2017-02-23 19:42:50 +0100

    Add files via upload

---
+++ js/javascript.js
@@ -0,0 +1,27 @@
+$(document).ready(function(){
+	$(".header-text").delay(1200).animate({opacity:1},800);
+});
+
+$(function(){
+	$('a[href=#]:not(a[href=#])').on('click',function(){
+		if(location.pathname.replace(/^\//,'')==this.pathname.replace(/^\//,'')&&location.hostname==this.hostname){
+			var target=$(this.hash);target=target.length?target:$('[name='+this.hash.slice(1)+']');
+			if(target.length){
+				$('html,body').animate({scrollTop:target.offset().top},900);
+				return false;
+			}
+		}
+	});
+});
+
+$(document).ready(function(){
+	$(window).scroll(function(){
+		$('.hideme').each(function(i){
+			var bottom_of_object=$(this).offset().top+$(this).outerHeight();
+			var bottom_of_window=$(window).scrollTop()+$(window).height();
+			if(bottom_of_window>bottom_of_object){
+				$(this).animate({'opacity':'1'},800);
+			}
+		});
+	});
+});
